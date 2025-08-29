7588ccadd8cc512451e895b03d8ecfb94251e05e
Dhirendra Yadav <dhirendra_yadav@epam.com>
2025-08-29 11:29:33 +0530

    Updating provider path & version

--- modules/snowflake_resources/_provider.tf
+++ modules/snowflake_resources/_provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "0.63.0"
+      source  = "snowflakedb/snowflake"
+      version = "1.0.4"
     }
   }
 }
--- staging/main.tf
+++ staging/main.tf
@@ -1,26 +1,26 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "0.63.0"
+      source  = "snowflakedb/snowflake"
+      version = "1.0.4"
     }
   }
 
-  backend "s3" {
-    bucket         = "<your-bucket-name>"
-    key            = "terraform-staging.tfstate"
-    region         = "<bucket-region>"
-    # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
-    # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
-  }
+  # backend "s3" {
+  #   bucket         = "<your-bucket-name>"
+  #   key            = "terraform-staging.tfstate"
+  #   region         = "<bucket-region>"
+  #   # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
+  #   # dynamodb_table = "terraform-state-lock-table"
+  #   encrypt        = true
+  #   role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
+  # }
 }
 
 provider "snowflake" {
-  username    = "<your_snowflake_username>"
-  account     = "<your_snowflake_account_identifier>"
-  role        = "<your_snowflake_role>"
+  username    = "DHIRENDRA"
+  account     = "DNJNTIC-AP22595.snowflakecomputing.com"
+  role        = "ACCOUNTADMIN"
   private_key = var.snowflake_private_key
 }
 
